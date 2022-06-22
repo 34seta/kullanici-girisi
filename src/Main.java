cf4b102490dc3e47c9c3139fc8988a137a45f4ee
Serdar Tayan <34seta@gmail.com>
2022-06-22 15:36:10 +0300

    parola sorgu

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        String userName;
-        String password;
+        String userName, newUserName;
+        String password, newPassword;
 
-        System.out.println("Lütfen Kullaci ismini giriniz:");
-        userName=input.nextLine();
-        System.out.println("Lütfen sifrenizi giriniz:");
-        password=input.nextLine();
+        System.out.println("Lutfen Kullaci ismini giriniz:");
+        userName = input.nextLine();
+        System.out.println("Lutfen sifrenizi giriniz:");
+        password = input.nextLine();
 
-        if (userName.equals("patika") && password.equals("java123")){
-            System.out.println("Şifre ve kullanici adiniz dogrudur. \n Başarili giris yatiniz");
-        } else if (!(userName.equals("patika")|| password.equals("java123"))) {
-            System.out.println("Sifre veya Kullanici adi hatali \n Sifrenizi unuttuysanız sıfırlamak için \n " +
-                    "yeni sifre ve kullanici aidnizi giriniz");
-
+        if (userName.equals("patika") && password.equals("java123")) {
+            System.out.println("Sifre ve kullanici adiniz dogrudur. \n Başarili giris yaptiniz");
+            
+        } else if (!(userName.equals("patika") || password.equals("java123"))) {
+            System.out.println("Sifre veya Kullanici adi hatali \n " +
+                    "Sifrenizi unuttuysanız sıfırlamak için");
         }
 
+        System.out.println("Yeni Kullanici adinizi giriniz");
+        newUserName = input.nextLine();
 
-        System.out.println("Hello world!");
+        System.out.println("Yeni Sifrenizi giriniz.");
+        newPassword = input.nextLine();
+
+        if (!(newPassword.equals("patika") && newUserName.equals("java123"))) {
+            System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+
+        } else {
+            System.out.println("Sifreniz Olusturuldu");
+        }
+
     }
+
+
 }
